2f94d71ec6251f7e68f25eede7d2a115f352b820
Degreet <degreetpro@gmail.com>
2023-05-22 19:36:38 +0300

    swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Travel Buddy API')
+    .setDescription(
+      'TravelBuddy is an API designed to facilitate travel and help tourists during their trips',
+    )
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
